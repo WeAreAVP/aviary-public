563535859afc108b7a4c4f9005bdee58c2cf40d0
David Agudelo-Frankel <36859994+davidagud@users.noreply.github.com>
2020-09-03 01:42:45 -0500

    Acc button fix (#39)
    * changed button text, color, alignment, and added shadow
    
    * added import statement
    
    * fixed accessibility button z index and action on submenu open
    
    Co-authored-by: Nouman Tayyab <noumantayyab@gmail.com>

--- app/assets/javascripts/accessibility.js
+++ app/assets/javascripts/accessibility.js
@@ -9,19 +9,30 @@ function Accessibility() {
         let button = $('#accessibility_button');
         var buttonHeight = button.outerHeight();
         var buttonOffset = $(window).height() - button[0].getBoundingClientRect().bottom;
+
+        var adjustLocation = function () {
+            let footer = $('.footer_section');
+            if (footer.length > 0) {
+                let footerOffset = $(window).height() - footer[0].getBoundingClientRect().top;
+                if (footerOffset >= buttonOffset + buttonHeight / 2) {
+                    button.css({bottom: +footerOffset - buttonHeight / 2 + 'px'})
+                } else {
+                    button.css({bottom: buttonOffset + 'px'})
+                }
+            }
+        };
+
         button.keydown(function (e) {
             if (e.keyCode == 32) {
                 e.target.click();
             }
         });
-        $(window).on('scroll resize load', function () {
-            let footer = $(".page-footer");
-            let footerOffset = $(window).height() - footer[0].getBoundingClientRect().top;
-            if (footerOffset >= buttonOffset + buttonHeight / 2) {
-                button.css({bottom: +footerOffset - buttonHeight / 2 + 'px'})
-            } else {
-                button.css({bottom: buttonOffset + 'px'})
-            }
+
+        $(window).on('scroll resize load', adjustLocation);
+
+        $(window).on('show.bs.collapse hide.bs.collapse', function() {
+            var timerId = setInterval(adjustLocation, 10);
+            setTimeout(function() {clearInterval(timerId)}, 500);
         });
     }
 }
