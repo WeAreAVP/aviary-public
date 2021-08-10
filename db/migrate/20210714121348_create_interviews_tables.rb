5ffce37a83f2e342552d23db7fda5f59ee684744
Furqan Wasi <furqanwasi@Furqans-MacBook-Pro.local>
2021-08-10 16:25:10 +0500

    done

--- db/migrate/20210714121348_create_interviews_tables.rb
+++ db/migrate/20210714121348_create_interviews_tables.rb
@@ -1,3 +1,5 @@
+# Aviary is an audiovisual content publishing platform with sophisticated features for search and permissions controls.
+# Copyright (C) 2019 Audio Visual Preservation Solutions, Inc.
 class CreateInterviewsTables < ActiveRecord::Migration[5.2]
   def change
     create_table :interviews do |t|
