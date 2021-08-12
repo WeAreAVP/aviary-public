d249eb8d95c2666a7ff4d1d82e057d0c2f9d0f31
furqan.wasi <furqanwasi@Furqans-MacBook-Pro.local>
2021-08-12 14:45:20 +0500

    aviary done

--- app/assets/javascripts/interview_manager.js
+++ app/assets/javascripts/interview_manager.js
@@ -95,6 +95,7 @@ function InterviewManager() {
             dropdown: true,
             scrollbar: true
         }).val(val);
+
         $('#interviews_interview_interview_date').datepicker();
         let containerRepeatManager = new ContainerRepeatManager();
         containerRepeatManager.makeContainerRepeatable(".add_keywords", ".remove_keywords", '.container_keywords_inner', '.container_keywords', '.keywords');
