2edaf979c26c0c379f5ba870fff763e42215b6a7
furqan.wasi <furqanwasi@Furqans-MacBook-Pro.local>
2021-08-26 18:37:58 +0500

    done

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
 # configuration to deploy the application on the staging server
 set :stage, :production
-set :branch, 'move_ohms_to_public'
+set :branch, 'master'
 
 set :server_name, 'public.aviaryplatform.com'
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
-server 'app/services/datatables/interviews_datatable.rb', user: 'deploy', roles: %w{web app db}, port: 22
+server '3.131.117.37', user: 'deploy', roles: %w{web app db}, port: 22
 
 set :deploy_to, -> { '/home/deploy/aviary' }
 set :rails_env, :production
