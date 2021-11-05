b3f32fa75cbd61c2c405438ff37a4c9bd20df555
furqan wasi <furqan@weareavp.com>
2021-11-05 17:38:44 +0500

    asd

--- db/migrate/20210924073707_alter_interview_for_index_status.rb
+++
@@ -1,7 +0,0 @@
-# Aviary is an audiovisual content publishing platform with sophisticated features for search and permissions controls.
-# Copyright (C) 2019 Audio Visual Preservation Solutions, Inc.
-class AlterInterviewForIndexStatus < ActiveRecord::Migration[5.2]
-  def change
-    add_column :interviews, :index_status, :integer, default: -1
-  end
-end
