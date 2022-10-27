94f2ec9eb3a0c0a822b8b684e13670bfdd1581e3
Raza Saleem <razasaleem1991@gmail.com>
2022-10-27 15:54:01 +0500

    AVIARY-3337_AVIARY-3345_AVIARY-3344_add_is_downloadable_in_transcripts (#231)
    * AVIARY-3337_AVIARY-3345_AVIARY-3344_add_is_downloadable_in_transcripts
    
    * AVIARY-3337_AVIARY-3345_AVIARY-3344_add_is_downloadable_in_transcripts
    
    * AVIARY-3337_AVIARY-3345_AVIARY-3344_add_is_downloadable_in_transcripts

---
+++ db/migrate/20221027075650_add_downloadable_to_transcript.rb
@@ -0,0 +1,24 @@
+class AddDownloadableToTranscript < ActiveRecord::Migration[6.1]
+  def change
+    add_column :file_transcripts, :is_downloadable, :integer, default: 1
+    Organization.all.each do |org|
+      transcript_display_column = JSON.parse(org.transcript_display_column) if org.transcript_display_column.present? 
+      transcript_search_column = JSON.parse(org.transcript_search_column) if org.transcript_search_column.present?
+      if transcript_display_column.present? && transcript_search_column.present?
+        display_keys = transcript_display_column['columns_status'].map { |_key, single_column| single_column['value'] }
+        search_keys = transcript_search_column.map { |_key, single_column| single_column['value'] }
+        begin
+          unless display_keys.include?('is_downloadable_ss')
+            transcript_display_column['columns_status'][transcript_display_column['columns_status'].count] = { 'status' => 'true', 'value' => 'is_downloadable_ss', 'sort_name' => 'true' }
+          end
+          unless display_keys.include?('is_downloadable_ss')
+            transcript_search_column[transcript_search_column.count] = { 'status' => 'true', 'value' => 'is_downloadable_ss' }
+          end
+          org.update(transcript_display_column: transcript_display_column.to_json, transcript_search_column: transcript_search_column.to_json)
+        rescue StandardError => exception
+          puts "\nUnable to update display and search columns for organization with id: #{org.id} and name: #{org.name}\nError => #{exception}" 
+        end
+      end
+    end
+  end
+end
