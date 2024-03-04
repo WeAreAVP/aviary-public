876d492e63db93665d574071f1484436e42c40ca
fayzan-cbr <147402375+fayzan-cbr@users.noreply.github.com>
2024-03-04 12:28:33 +0500

    Sprint-145 (#377)
    * AVIARY-4396 | Modify Aviary fix line breaks logic
    
    * Transcript editor new release
    
    * Allow plain txt transcripts to be edited using transcript editor
    
    * updated license infomation
    
    ---------
    
    Co-authored-by: Nouman Tayyab <noumantayyab@gmail.com>

---
+++ public/transcript-editor/precache-manifest.6be05428e52b46c264c01dd764fed1fa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b338b08087443b43cbcacc121500c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1a2fcd61bec5277ae3e2",
+    "url": "./static/css/2.a1aa72a9.chunk.css"
+  },
+  {
+    "revision": "e4bc209dd8692e96e36b",
+    "url": "./static/css/main.32caac85.chunk.css"
+  },
+  {
+    "revision": "1a2fcd61bec5277ae3e2",
+    "url": "./static/js/2.176292af.chunk.js"
+  },
+  {
+    "revision": "2d04cdf317cba3c429199a473acae6ea",
+    "url": "./static/js/2.176292af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4bc209dd8692e96e36b",
+    "url": "./static/js/main.7d60adda.chunk.js"
+  },
+  {
+    "revision": "5d4623be92d53248c8e9",
+    "url": "./static/js/runtime-main.44b9a2af.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "./static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "./static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "./static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "./static/media/notification.c0d3c94c.eot"
+  }
+]);
