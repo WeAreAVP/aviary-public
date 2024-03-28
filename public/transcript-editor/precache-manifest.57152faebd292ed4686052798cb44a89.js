6c3687d0b515f40babab7ce1f51f9b6b2f0960f0
fayzan-cbr <fayzan@cybernest.com>
2024-03-28 14:03:06 +0500

    Optimized transcript editor

---
+++ public/transcript-editor/precache-manifest.57152faebd292ed4686052798cb44a89.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef55a40fbc9942bf4e73ac1cd146ac9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e767981bb15831492135",
+    "url": "./static/css/2.a1aa72a9.chunk.css"
+  },
+  {
+    "revision": "63e1647f64caa3c1d145",
+    "url": "./static/css/main.32caac85.chunk.css"
+  },
+  {
+    "revision": "e767981bb15831492135",
+    "url": "./static/js/2.72d843ef.chunk.js"
+  },
+  {
+    "revision": "2d04cdf317cba3c429199a473acae6ea",
+    "url": "./static/js/2.72d843ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e1647f64caa3c1d145",
+    "url": "./static/js/main.9a82cec3.chunk.js"
+  },
+  {
+    "revision": "5d4623be92d53248c8e9",
+    "url": "./static/js/runtime-main.44b9a2af.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "./static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "./static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "./static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "./static/media/notification.c0d3c94c.eot"
+  }
+]);
--- public/transcript-editor/service-worker.js
+++ public/transcript-editor/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3713092fd29fd153a0a99c9b40bc4e04.js"
+  "./precache-manifest.57152faebd292ed4686052798cb44a89.js"
 );
 
 self.addEventListener('message', (event) => {
