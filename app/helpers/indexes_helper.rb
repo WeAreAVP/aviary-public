f06d91766f395764bfaca3dadc76f33c36f239f8
Usman Javaid <usmanjzcn@gmail.com>
2024-07-05 15:19:27 +0500

    sprint-154-usman

--- app/helpers/indexes_helper.rb
+++ app/helpers/indexes_helper.rb
@@ -13,7 +13,22 @@ module IndexesHelper
   end
 
   def update_skip_duration
-    session['index_update_backward_duration'] = params['index_update_backward_duration'].presence || session['index_update_backward_duration']
-    session['index_update_forward_duration'] = params['index_update_forward_duration'].presence || session['index_update_forward_duration']
+    session['index_update_backward_duration'] = params['index_update_backward_duration'].presence ||
+                                                session['index_update_backward_duration']
+    session['index_update_forward_duration'] = params['index_update_forward_duration'].presence ||
+                                               session['index_update_forward_duration']
+  end
+
+  def skip_duration_options
+    [{ duration: 1, label: '1s' }, { duration: 10, label: '10s' }, { duration: 15, label: '15s' },
+     { duration: 20, label: '20s' }, { duration: 30, label: '30s' }, { duration: 45, label: '45s' },
+     { duration: 50, label: '50s' }, { duration: 60, label: '60s' }, { duration: 300, label: '5m' }]
+  end
+
+  def skip_duration_options_html(target)
+    skip_duration_options.map do |option|
+      "<li><a href='javascript:void(0)' class='dropdown-item update_duration_option' data-target='#{target}' " \
+        "data-value='#{option[:duration]}'> #{option[:label]}</a></li>"
+    end.join
   end
 end
